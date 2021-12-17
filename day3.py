b9c905c253c9fe6be733396da269c6f7af85a83c
Michael Scott <thebluenowhere@protonmail.com>
2021-12-17 14:16:36 +0000

    headache

--- day3.py
+++ day3.py
@@ -8,31 +8,37 @@ epsilon = []
 list = []
 
 for data in i:
-    dig1 = data[0]
+    dig0 = data[0]
+    dig1 = data[1]
+    dig2 = data[2]
+    dig3 = data[3]
+    dig4 = data[4]
+    dig5 = data[5]
+    dig6 = data[6]
+    dig7 = data[7]
+    dig8 = data[8]
+    dig9 = data[9]
     list.append(data[0])
+    list.append(data[1])
+    list.append(data[2])
+    list.append(data[3])
+    list.append(data[4])
+    list.append(data[5])
+    list.append(data[6])
+    list.append(data[7])
+    list.append(data[8])
+    list.append(data[9])
 
-index1a = list.count('0')
-index1b = list.count('1')
+index0a = list.count('0')
+index0b = list.count('1')
 
-if index1a > index1b:
+if index0a > index0b:
     gamma.append(0)
     epsilon.append(1)
+else:
+    gamma.append(1)
+    epsilon.append(0)
+
 
 print('The gamma rate is: ' + str(gamma))
 print('The epsilon rate is: ' + str(epsilon))
-#
-#def f(index):
-#    for data in i:
-#        gamma = []
-#        epsilon = []
-#        list = []
-#        dig = data[index]
-#        list.append(data)#
-
-#        index1a = list.count('0')
-#        index1b = list.count('1')
-
-#        if index1a > index1b:
-#            gamma.append(0)
-#            epsilon.append(1)
-#print(f(0))
