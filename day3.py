57e8d88f7cc849255a8eb3125121a9f55b757909
Michael Scott <thebluenowhere@protonmail.com>
2021-12-17 14:44:41 +0000

    fucked this up

--- day3.py
+++ day3.py
@@ -5,29 +5,24 @@ i = [data.strip() for data in file.readlines()]
 
 gamma = []
 epsilon = []
-list = []
 
-for data in i:
+def gamma(i):
+    zeroCount = 0
+    oneCount = 1
+    for x in lines:
+        if x[i] == '0':
+            zeroCount += 1
+        else:
+            oneCount += 1
+        if zeroCount > oneCount:
+            return '0'
+        else:
+            return '1'
+
+
+
     dig0 = data[0]
-    dig1 = data[1]
-    dig2 = data[2]
-    dig3 = data[3]
-    dig4 = data[4]
-    dig5 = data[5]
-    dig6 = data[6]
-    dig7 = data[7]
-    dig8 = data[8]
-    dig9 = data[9]
     list.append(data[0])
-    list.append(data[1])
-    list.append(data[2])
-    list.append(data[3])
-    list.append(data[4])
-    list.append(data[5])
-    list.append(data[6])
-    list.append(data[7])
-    list.append(data[8])
-    list.append(data[9])
 
 index0a = list.count('0')
 index0b = list.count('1')
