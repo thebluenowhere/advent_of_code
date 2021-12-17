f9fba2f01faf320ee7fbf0b0ba046f0a10ef4c9c
Michael Scott <thebluenowhere@protonmail.com>
2021-12-17 10:52:39 +0000

    day 3 progression

--- day3.py
+++ day3.py
@@ -3,19 +3,20 @@
 file = open('input3.txt', 'r')
 i = [data.strip() for data in file.readlines()]
 
-binary = []
+gamma = []
+epsilon = []
 list = []
+
 for data in i:
     dig1 = data[0]
     list.append(data[0])
 
-index1a = list.count(0)
-index1b = list.count(1)
+index1a = list.count('0')
+index1b = list.count('1')
 
 if index1a > index1b:
-    binary.append(index1a)
-else:
-    binary.append(index1b)
+    gamma.append(0)
+    epsilon.append(1)
 
-print(binary)
-
+print('The gamma rate is: ' + str(gamma))
+print('The epsilon rate is: ' + str(epsilon))
