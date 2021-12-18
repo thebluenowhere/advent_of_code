3444027713e93881fb370de811e8ccf9121d8cb3
Michael Scott <thebluenowhere@protonmail.com>
2021-12-18 12:21:31 +0000

    warmer

--- day3.py
+++ day3.py
@@ -2,23 +2,21 @@
 
 file = open('input3.txt', 'r')
 lines = [data.strip() for data in file.readlines()]
-
+print(lines)
 gamma = ""
 epsilon = ""
 digits = ""
 
-for i in range(12):
-    for x in lines[i]:
-        digits = digits + str(lines[i])
-        zero = digits.count('0')
-        ones = digits.count('1')
-        if zero > ones:
-            gamma += '0'
-            epsilon += '1'
-        else:
-            gamma += '1'
-            epsilon += '0'
-        digits = ""
+for i in lines[i]:
 
-print(gamma)
-print(epsilon)
+    digits = digits + str(lines[i])
+    #print(digits)
+    zero = digits.count('0')
+    ones = digits.count('1')
+    if zero > ones:
+        gamma += '0'
+        epsilon += '1'
+    else:
+        gamma += '1'
+        epsilon += '0'
+    digits = ""
