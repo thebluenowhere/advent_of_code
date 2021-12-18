e603640dc4072bf0b7593ba9174850c3aff2e8b4
Michael Scott <thebluenowhere@protonmail.com>
2021-12-18 14:34:14 +0000

    more work

--- day3.py
+++ day3.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 
 file = open('input3.txt', 'r')
-lines = [data.strip() for data in file.readlines()]
-print(lines)
+fileOpen = [data.strip() for data in file.readlines()]
 gamma = ""
 epsilon = ""
 digits = ""
-i = lines[0]
 
-for lines in range(len(lines)):
+for lines in fileOpen:
+    i = lines[0]
     for lines[i] in range(12):
-       digits = digits + lines[i]
-       zero = digits.count('0')
-       ones = digits.count('1')
-       if zero > ones:
-           gamma += '0'
-           epsilon += '1'
-       else:
-           gamme += '1'
-           epsilon += '0'
-           digits = ""
-
+        digits = digist + lines[i]
+        print(digits)
+        zero = digits.count('0')
+        ones = digits.count('1')
+        if zero > ones:
+            gamma += '0'
+            epsilon += '1'
+        else:
+            gamme += '1'
+            epsilon += '0'
+            digits = ""
